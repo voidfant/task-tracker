90f035ff237acbdd724d14e2a7a725f5bae5424c
Konstantin Savchenko <nuarielq@yandex.ru>
2023-02-08 14:11:59 +0300

    crud basics

--- backend/db/database.py
+++ backend/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:7786@localhost:5433/alchemy"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://konstantin:7786@localhost:5432/alchemy"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
--- backend/routers/route.py
+++ backend/routers/route.py
@@ -15,3 +15,5 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
 @router.get("/users/me/items/")
 async def read_own_items(current_user: User = Depends(get_current_active_user)):
     return [{"item_id": "Foo", "ownr": current_user.username}]
+
+
